578110014f979d32728e76807e6810dfca60e03f
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-09-10 15:26:41 +0800

    Add files via upload

--- src/pages/score.jsx
+++ src/pages/score.jsx
@@ -1,5 +1,48 @@
 import * as React from 'react'
+import TopBar from '../Topbar'
 
-export function Score({ data }) {
+export function Score({ data, user }) {
 
+  const [scorelist, setScoreList] = React.useState([
+    { title: "", id: "" }
+  ])
+
+  function getScore() {
+    fetch("/api/getscore", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+
+        fetch("/api/getscoremap", {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({}),
+        })
+          .then(res2 => res2.json())
+          .then(res2 => {
+            console.log(res2)
+            var list = []
+            for (let i = 0; i < res2.data.result.length; i++) {
+              list.push({ title: res2.data.result[i].scoreName, id: res2.data.result[i].uid })
+            }
+            setScoreList(list)
+          })
+      })
+  }
+
+  React.useEffect(() => {
+    getScore()
+  }, [])
+
+  return (
+    <TopBar logined={true} data={data.data} user={user} title={"首頁"} />
+  )
 }
