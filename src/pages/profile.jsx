7e65efb16a1091fe74936dad7169935c8617be83
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-09-23 20:47:55 +0800

    Add files via upload

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
 import TopBar from '../Topbar'
-import { Box, Button } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import "../App.css"
 import { red, yellow, green } from '@mui/material/colors';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
 import { styled } from '@mui/material/styles';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Link } from 'react-router-dom';
 import FaceIcon from '@mui/icons-material/Face';
+import Chip from '@mui/material/Chip';
 
 export function Profile({ data, user }) {
 
@@ -34,9 +34,30 @@ export function Profile({ data, user }) {
             <Box sx={{ p: 3 }}>
                 <h1>個人資料</h1>
 
-                <Paper sx={{ display: "flex" }}>
-                    <FaceIcon />
-
+                <Paper>
+                    <div style={{ display: "flex" }}>
+                        <FaceIcon />
+                        <h2>{data.data.username}
+                            &nbsp;
+                            {
+                                data.data.role === "std" ?
+                                    <Chip label="學生" color="primary" />
+                                    :
+                                    data.data.role === "par" ?
+                                        <Chip label="家長" color="secondary" />
+                                        :
+                                        <Chip label="老師" color="success" />
+                            }</h2>
+                        <h3>{data.data.userid}</h3>
+                    </div>
+                </Paper>
+                <p></p>
+                <Paper>
+                    <h2>變更密碼</h2>
+                    <TextField type='password' label="輸入原本密碼" />
+                    <TextField type='password' label="輸入新密碼" />
+                    <TextField type='password' label="再次輸入新密碼" />
+                    <Button variant='contained'>送出</Button>
                 </Paper>
 
             </Box>
