d09a55c5c0f3e06c0f753a052d954f45d60f78f4
jerry20201230 <jerry20200815@gmail.com>
2023-09-24 21:47:19 +0800

    :K

--- src/pages/searchScores.jsx
+++ src/pages/searchScores.jsx
@@ -46,7 +46,7 @@ export function SearchScoreSheet({ data, user }) {
 				console.log(res2)
 				var list = []
 				for (let i = 0; i < res2.data.result.length; i++) {
-					list.push({ title: res2.data.result[i].scoreName, id: res2.data.result[i].uid })
+					list.push({ title: res2.data.result[i].scoreName, id: res2.data.result[i].uid, subject: res2.data.result[i].subject })
 				}
 				setScoreList(list)
 			})
