40e552bb8fe042e14d67634018653404041c8b4b
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-10-24 14:04:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -439,10 +439,11 @@ app.post("/api/getscorebyid", (req, res) => {
                       sql_Connect.getConnection(function (err, connection4) {
                         connection4.query(`
                       UPDATE parentAccountCtrl
-                      SET ${req.body.id} = ${Number(results3[0][req.body.id].split("%|%")[0]) + 1}%|%${dayjs(new Date()).format("YYYY/MM/DD HH:mm:ss")}"
+                      SET ${req.body.id} = "${Number(results3[0][req.body.id].split("%|%")[0]) + 1}%|%${dayjs(new Date()).format("YYYY/MM/DD HH:mm:ss")}"
                       WHERE stdId = ${req.session.userid};
                     `, function (error4, results4, fields4) {
                           console.log("parent data uploaded")
+                          console.log(`${Number(results3[0][req.body.id].split("%|%")[0]) + 1}%|%${dayjs(new Date()).format("YYYY/MM/DD HH:mm:ss")}`)
                           connection4.release()
                         })
                       })
