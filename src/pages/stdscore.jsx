06ff7986e1f271e5c02df0e93455e97b85ba1db0
jerry20201230 <jerry20200815@gmail.com>
2023-10-26 22:07:53 +0800

    /

--- src/pages/stdscore.jsx
+++ src/pages/stdscore.jsx
@@ -199,7 +199,7 @@ export function StdScore({ data, user }) {
               <ListItem>
 
                 <ListItemText id="switch-list-label-wifi" primary="短暫維持家庭和睦"
-                  secondary={<>暫停家長查詢這筆成績的權限10分鐘，期間將在家長的裝置上顯示錯誤訊息。<br />每筆成績每天限用3次，你今天用了</>} />
+                ></ListItemText>
                 <Switch
                   edge="end"
                   onChange={() => {
@@ -209,6 +209,7 @@ export function StdScore({ data, user }) {
                   }}
                   checked={setting_1}
                 />
+                <ListItemText secondary={<>暫停家長查詢這筆成績的權限10分鐘，期間將在家長的裝置上顯示錯誤訊息。<br />每筆成績每天限用3次，你今天還有{scoreData.queryTimes.split("%|%")[2]}次機會</>} />
               </ListItem>
             </List>
           </Paper>
