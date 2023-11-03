0841d7df9c1ed948566391cf765a6dd58e005e23
jerry20201230 <jerry20200815@gmail.com>
2023-11-03 18:39:09 +0800

    /

--- server.js
+++ server.js
@@ -362,7 +362,7 @@ app.post("/api/deletescore", (req, res) => {
 
 app.post("/api/uploadnewscore", (req, res) => {
   if (req.session.role === "teacher") {
-    var theUUID = uuidv4().slice(0, 7).replace("e", "k")
+    var theUUID = "1234567"//uuidv4().slice(0, 7).replace("e", "k")
     //create uuid
     //add new column
     //put all data
