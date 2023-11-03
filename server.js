fab096276e85e79ee76bcc5b03f75bf4ff8c8fe2
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2023-11-03 08:16:10 +0800

    /

--- server.js
+++ server.js
@@ -382,6 +382,7 @@ app.post("/api/uploadnewscore", (req, res) => {
     //   }
     // }
     while (Number(theUUID) !== NaN) {
+      console.log("UUID",theUUID)
       theUUID = uuidv4().slice(0, 7)
     }
 
@@ -825,7 +826,7 @@ var connectionTest =
             sql_Connect.getConnection(function (err, connection3) {
               connection3.query(`
             INSERT INTO connectionTest (k,time)
-            VALUES(${1},${dayjs().format("YYYY/MM/DD HH:mm:ss")})
+            VALUES(${1},"${dayjs().format("YYYY/MM/DD HH:mm:ss")}")
 
         `, function (error3, results3, fields) {
                 if (error3) {
