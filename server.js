8d5fe69db3f1a7a9485b18bae65dbf2231199776
jerry20201230 <jerry20200815@gmail.com>
2023-11-05 21:20:43 +0800

    /

--- server.js
+++ server.js
@@ -839,7 +839,7 @@ app.post("/api/sqltest", (req, res) => {
           sql_Connect.getConnection(function (err, connection3) {
             connection3.query(`
             INSERT INTO connectionTest (k,time)
-            VALUES(${1},"${dayjs().add(8, "hours").format("YYYY/MM/DD HH:mm:ss")}")
+            VALUES(${0},"${dayjs().add(8, "hours").format("YYYY/MM/DD HH:mm:ss")}")
 
         `, function (error3, results3, fields) {
               if (error3) {
